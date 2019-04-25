e5bfc97d1e8ffbfe2a0f7b8e8baec5dafccb07e2
Clément Auger <clement.auger@epitech.eu>
2019-04-25 11:45:49 +0200

    2 step

--- calcul.py
+++ calcul.py
@@ -4,14 +4,13 @@ from math import pi, sqrt, exp
 import time
 
 def printNbValues(nb):
-    print("\tNumber of values:\t", nb, sep='')
+    print("    Number of values:\t", nb, sep='')
 
 def printStdDev(value, nb, stdDev):
+
     return stdDev
 
 def printArithmMean(value, nb, arithmMean):
     newArithmMean = (arithmMean * nb + value) / (nb + 1)
-    print("\tArithmetic mean:\t", "%.2f" % newArithmMean)
+    print("    Arithmetic mean:\t", "%.2f" % newArithmMean)
     return newArithmMean
-
-#206 : moy n + 1 = (moyenne n * n + nouvel elem) / n + 1
